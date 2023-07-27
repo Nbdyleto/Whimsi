67e8ea1a408b2dc352952d0d312c67ce3ca6b80e
Nbdyleto <nobodyleto2004@gmail.com>
2023-07-27 03:25:41 -0300

    Show highlighted properties in template

--- src/whimsi/views.py
+++ src/whimsi/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import HighlightedProperty
 
 def home_view(request):
-    return render(request, 'templates/pages/home.html', {})
+    highlighted_properties = HighlightedProperty.objects.prefetch_related('property__propertyaddress').all()
+
+    context = {
+        'highlighted_properties': highlighted_properties,
+    }
+    
+    return render(request, 'templates/pages/home.html', context)
