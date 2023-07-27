5771e6942ec911049ff9d127c458266597b6ddac
Nbdyleto <nobodyleto2004@gmail.com>
2023-07-27 03:54:22 -0300

    Show latest properties in template

--- src/whimsi/views.py
+++ src/whimsi/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from .models import HighlightedProperty
+from .models import Property, HighlightedProperty
 
 def home_view(request):
+    last_properties = Property.objects.order_by('-created_at')[:3]
+    print('last_properties', last_properties)
     highlighted_properties = HighlightedProperty.objects.prefetch_related('property__propertyaddress').all()
 
     context = {
+        'last_properties': last_properties,
         'highlighted_properties': highlighted_properties,
     }
     
